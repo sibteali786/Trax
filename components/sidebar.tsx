beaeb8c44b3294f9bff3139931ccf2b12c1c11be
sibteali786 <sibteali786@gmail.com>
2022-07-31 15:52:16 +0500

    added basic layout

---
+++ components/sidebar.tsx
@@ -0,0 +1,29 @@
+import NextImage from 'next/image'
+import {
+    Box,
+    List,
+    ListItem,
+    ListIcon,
+    Divider,
+    Center,
+    LinkBox,
+    LinkOverlay
+} from '@chakra-ui/layout'
+import {
+    MdHome,
+    MdSearch,
+    MdLibraryMusic,
+    MdPlaylistAdd,
+    MdFavorite
+} from 'react-icons/md'
+const Sidebar = () => {
+    return (
+        <Box width='100%' height='calc(100vh - 100px)' bg='black' paddingX='5px' color='gray'>
+            <Box width='120px' marginBottom='20px' paddingX='20px'>
+                <NextImage src='/logo.svg' height={60} width={120} />
+            </Box>
+        </Box>
+    );
+}
+
+export default Sidebar;
