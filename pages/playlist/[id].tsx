37ce100ece90d6ebe253ec14fede2242671fbf6b
Syed_Sibteali_Baqar <sibteali786@gmail.com>
2022-08-28 01:57:16 +0500

    adjusted playlist UI

--- pages/playlist/[id].tsx
+++ pages/playlist/[id].tsx
@@ -1,6 +1,7 @@
 import { validateToken } from "../../lib/auth";
 import prisma from "../../lib/prisma";
 import { InferGetServerSidePropsType } from "next";
+import GradientLayout from "../../components/gradientLayout";
 
 export const getServerSideProps = async ({ query, req }) => {
   const { id } = validateToken(req.cookies.TRAX_ACCESS_TOKEN);
@@ -28,10 +29,38 @@ export const getServerSideProps = async ({ query, req }) => {
   };
 };
 
+const getBGColor = (id) => {
+  const colors = [
+    "red",
+    "green",
+    "blue",
+    "orange",
+    "purple",
+    "gray",
+    "teal",
+    "yellow",
+  ];
+
+  return colors[id - 1] || colors[Math.floor(Math.random() * colors.length)];
+};
+
 const Playlist = ({
   playlist,
 }: InferGetServerSidePropsType<typeof getServerSideProps>) => {
-  return <div>{playlist.name}</div>;
+  const color = getBGColor(playlist.id);
+
+  return (
+    <GradientLayout
+      color={color}
+      roundImage={false}
+      title={playlist.name}
+      subtitle="playlist"
+      description={`${playlist.songs.length} songs`}
+      image={`https://picsum.photos/400?random=${playlist.id}`}
+    >
+      <div>dfd</div>
+    </GradientLayout>
+  );
 };
 
 export default Playlist;
